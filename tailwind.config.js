4e0982ca9f23084e1e02e8c7b7b726fe06a15155
xxyylloo <schmitzberger.luca@johak.at>
2024-11-15 11:07:59 +0100

    added daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,5 @@ export default {
 		extend: {}
 	},
 
-	plugins: [typography, forms, containerQueries, aspectRatio]
+	plugins: [typography, forms, containerQueries, aspectRatio,require('daisyui'),]
 };
