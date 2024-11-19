8bd296ff7157a8cd22104361c34552b79f58a70e
xxyylloo <schmitzberger.luca@johak.at>
2024-11-19 11:09:30 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1 class="text-red-950">Welcome to fronted-project</h1>
+<h1 class="text-white">Welcome to papichler</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
